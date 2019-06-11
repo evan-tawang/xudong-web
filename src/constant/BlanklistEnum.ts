7dda3b0329b3626547d59bef70322f93767e8018
沈炜 <277469513@qq.com>
2019-06-11 20:57:43 +0800

    黑名单

---
+++ src/constant/BlanklistEnum.ts
@@ -0,0 +1,8 @@
+const BlanklistEnum = {
+    Status: {
+        NORMAL: {value: 1, text: '正常',color: 'green'},
+        STOP: {value: 2, text: '停用',color: 'orange'},
+    }
+};
+
+export default BlanklistEnum;
--- src/views/blanklist/BlankList.vue
+++ src/views/blanklist/BlankList.vue
@@ -5,6 +5,7 @@
     import {Getter, Action} from "vuex-class";
     import RouterName from "@/constant/router-name";
     import {BlankListQuery} from "@/model";
+    import BlanklistEnum from "@/constant/BlanklistEnum";
 
     import Api from "@/api";
 
@@ -13,9 +14,11 @@
 
     @Component
     export default class BlankList extends Vue {
-        private page: any = {};
-        private list: object = {};
+        private page: number = 0;
+        private list: object = [];
         private query: any = DEFAULT_QUERY;
+        private loading: any = {list: false, stopUsing: false, startUsing: false, remove: false};
+        private selectedDatas: any = [];
 
         @Action("blankList") private blankList: any;
 
@@ -27,9 +30,12 @@
          * 查询
          */
         private getListData() {
+            this.loading.list = true;
             Api.$get(API_URL_PREFIX + "/list", this.query).then((res: any) => {
-                //console.log(res.data);
+                console.log(res.data);
                 this.list = res.data;
+                this.page = res.page;
+                this.loading.list = false;
             });
         }
 
@@ -37,6 +43,7 @@
          * 查询
          */
         private searchData() {
+            this.query.pageNo = DEFAULT_QUERY.pageNo;
             this.getListData();
         }
 
@@ -44,11 +51,12 @@
          * 清空
          */
         private resetQuery() {
-            this.query = DEFAULT_QUERY;
+            this.query = {pageNo: DEFAULT_QUERY.pageNo, pageSize: DEFAULT_QUERY.pageSize};
             this.getListData();
         }
 
         private pageSizeChange(val: number) {
+            this.query.pageNo = DEFAULT_QUERY.pageNo;
             this.query.pageSize = val;
             this.getListData();
 
@@ -67,18 +75,76 @@
 
         }
 
-        private startUsing() {
+        private selectListRow(val: any) {
+            this.selectedDatas = val;
+        }
 
+        private updateStatus(operateText: string, newStatus: number) {
+            if (this.selectedDatas.length == 0) {
+                this.$alert("请选择需要 [" + operateText + "] 的黑名单！", {
+                    type: "warning",
+                    showClose: false
+                });
+            } else {
+                var _this = this;
+                this.$confirm("确定 [" + operateText + "] 所选黑名单?", {
+                    // confirmButtonText: '确定',
+                    // cancelButtonText: '取消',
+                    type: "info",
+                    showClose: false
+                }).then(() => {
+                    var ids: string = "";
+                    //var count = this.selectedDatas.length;
+                    this.selectedDatas.forEach(function(e: any) {
+                        //ids.push(e.id);
+                        ids += "," + e.id;
+                    });
+                    ids = ids.substr(1);
+                    //console.log(ids);
+                    Api.$post(API_URL_PREFIX + "/updateStatusGroup", {
+                        "ids": ids,
+                        "newStatus": newStatus
+                    }).then((res: any) => {
+                        this.$message({
+                            type: "success",
+                            message: "操作成功!",
+                            duration: 1000,
+                            onClose: function() {
+                                _this.getListData();
+                            }
+                        });
+                    });
+                });
+            }
+        }
+
+        private startUsing() {
+            this.updateStatus("启用", BlanklistEnum.Status.NORMAL.value);
         }
 
         private stopUsing() {
-
+            this.updateStatus("停用", BlanklistEnum.Status.STOP.value);
         }
 
         private remove(id: number) {
-
+            this.$confirm("确定 [删除] 该黑名单?", {
+                // confirmButtonText: '确定',
+                // cancelButtonText: '取消',
+                type: "warning",
+                showClose: false
+            }).then(() => {
+                var _this = this;
+                Api.$post(API_URL_PREFIX + "/delete", {id: id}).then((res: any) => {
+                    this.$message({
+                        type: "success",
+                        message: "操作成功!",
+                        duration: 1000,
+                        onClose: function() {
+                            _this.getListData();
+                        }
+                    });
+                });
+            });
         }
-
-
     }
 </script>

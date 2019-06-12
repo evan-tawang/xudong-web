227d031a0fee87e051356696d017ccefe6e9f3f7
沈炜 <277469513@qq.com>
2019-06-12 20:56:04 +0800

    post提交改成form提交

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,15 @@ import axios from './axios';
 class Api {
     public $post(url: string, params: any = {}) {
         return new Promise((resolve, reject) => {
-            axios.post(url, null, {params}).then((res: any) => {
+
+            let data = new FormData();
+
+            for(let k in params){
+                data.append(k,params[k]);
+            }
+
+            axios.post(url, data).then((res: any) => {
+            //axios.post(url, null, {params}).then((res: any) => {
                 resolve(res);
             }).catch((err: any) => {
                 reject(err);

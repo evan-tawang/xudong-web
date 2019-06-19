9659b5595281b149879559231ce8fa7efeb93243
沈炜 <277469513@qq.com>
2019-06-19 16:48:02 +0800

    暂时隐藏客服分配功能

--- src/constant/menus.ts
+++ src/constant/menus.ts
@@ -2,7 +2,7 @@ import RouterName from '@/constant/router-name';
 
 const Menus = [
 	{icon: 'el-icon-menu', text: '聊天', routerName: RouterName.STAFF_CHAT},
-	{icon: 'el-icon-menu', text: '客服分配', routerName: RouterName.ALLOCATE},
+	//{icon: 'el-icon-menu', text: '客服分配', routerName: RouterName.ALLOCATE},
 	{icon: 'el-icon-menu', text: '黑名单设置', routerName: RouterName.BLACKLIST},
 	{icon: 'el-icon-menu', text: '敏感词设置', routerName: RouterName.SENSITIVE_WORD},
 	{icon: 'el-icon-menu', text: '常用话术设置', routerName: RouterName.TALK_SKILL_LIST},

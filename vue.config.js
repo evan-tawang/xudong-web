69bebcdda729be96851f5a4e8ceb1f8e659b0fdb
mouhaining <>
2019-07-02 15:00:44 +0800

    整理对外文件

--- vue.config.js
+++ vue.config.js
@@ -66,8 +66,8 @@ module.exports = {
         proxy: {//代理
             '/': {
                 ws: false,
-                // target: 'http://127.0.0.1:9001',
-                target: 'http://47.96.89.84:9001',
+                target: 'http://127.0.0.1:9001',
+                // target: 'http://47.96.89.84:9001',
                 changeOrigin: true,
                 pathRewrite: {
                     '/service': '',
